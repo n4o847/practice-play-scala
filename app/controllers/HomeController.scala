df05e14eb089b853f4aa5928831cd1e408ce111f
n4o847 <22975590+n4o847@users.noreply.github.com>
2020-03-31 02:24:44 +0900

    get a name as a query parameter and print it

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,24 +1,26 @@
 package controllers
 
-import javax.inject._
-import play.api._
-import play.api.mvc._
+import javax.inject.Inject
+import javax.inject.Singleton
+import play.api.mvc.Action
+import play.api.mvc.AnyContent
+import play.api.mvc.BaseController
+import play.api.mvc.ControllerComponents
+import play.api.mvc.Request
 
-/**
- * This controller creates an `Action` to handle HTTP requests to the
- * application's home page.
- */
 @Singleton
-class HomeController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+class HomeController @Inject() (val controllerComponents: ControllerComponents)
+    extends BaseController {
 
-  /**
-   * Create an Action to render an HTML page.
-   *
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
-  def index() = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.index())
+  def index(maybeName: Option[String]) = Action {
+    implicit request: Request[AnyContent] =>
+      Ok {
+        val message = maybeName
+          .map(name => s"Hello, $name!")
+          .getOrElse(
+            """Please give a name as a query parameter named "name"."""
+          )
+        views.html.index(message)
+      }
   }
 }
--- build.sbt
+++ build.sbt
@@ -1,7 +1,7 @@
-name := """practice-play-scala"""
+name := "practice-play-scala"
 organization := "com.example"
 
-version := "1.0-SNAPSHOT"
+version := "0.1.0"
 
 lazy val root = (project in file(".")).enablePlugins(PlayScala)
 
@@ -9,9 +9,3 @@ scalaVersion := "2.13.1"
 
 libraryDependencies += guice
 libraryDependencies += "org.scalatestplus.play" %% "scalatestplus-play" % "5.0.0" % Test
-
-// Adds additional packages into Twirl
-//TwirlKeys.templateImports += "com.example.controllers._"
-
-// Adds additional packages into conf/routes
-// play.sbt.routes.RoutesKeys.routesImport += "com.example.binders._"
